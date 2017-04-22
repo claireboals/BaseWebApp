35f543a5b05b70ffebfee96362658549c956771a
claireboals <claireboals@gmail.com>
2017-04-22 12:02:43 -0400

    danny d fanpage

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,7 @@ function myFunction() {
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // jQuery can do a lot of crazy stuff so make sure to google around to find out more
 
-  $("#demo").html("holt sucks");
+  $("#demo").html("danny d");
 
   // 'img-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
   $("#dennett-image").append(`<img class="img-circle" src="/images/dennettsanta1.jpg" />`);
